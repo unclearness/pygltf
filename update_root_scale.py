6682497ce1bbd5523cdb0e4fd69fd6dcf628b5c0
unlclearness <unclearness.dev@gmail.com>
2022-03-21 14:08:30 +0900

    Update

--- update_root_scale.py
+++ update_root_scale.py
@@ -3,7 +3,7 @@ import json
 import argparse
 
 
-def updateScale(gltf, dst_size):
+def updateGeometryScale(gltf, dst_size):
     # Get taget node
     node = gltf["nodes"][0]
 
@@ -35,6 +35,41 @@ def updateScale(gltf, dst_size):
     node["scale"] = scale
 
     gltf["nodes"][0] = node
+    return gltf
+
+
+def updateScale(gltf, dst_size):
+    # Get min/max and merge for each geometry
+    merged_max = [-inf, -inf, -inf]
+    merged_min = [inf, inf, inf]
+    for acc in gltf["accessors"]:
+        # Accessors specified by
+        # meshes[i].primitives.attributes.POSITION
+        # must have max/min
+        if "max" in acc:
+            cur_max = acc["max"]
+            for i in range(3):
+                if merged_max[i] < cur_max[i]:
+                    merged_max[i] = cur_max[i]
+        if "min" in acc:
+            cur_min = acc["min"]
+            for i in range(3):
+                if cur_min[i] < merged_min[i]:
+                    merged_min[i] = cur_min[i]
+    org_size = []
+    for i in range(3):
+        org_size.append(merged_max[i] - merged_min[i])
+
+    # Compute scale
+    scale = []
+    for i in range(3):
+        scale.append(dst_size[i] / org_size[i])
+    root_node = {
+        "name": "root",
+        "scale": scale,
+        "children": [1]
+    }
+    gltf["nodes"].insert(0, root_node)
     return gltf
 
 
